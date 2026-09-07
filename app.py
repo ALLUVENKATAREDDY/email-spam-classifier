b9f975fd44ce69af6b8e94c510e8be393c6c4fdd
ALLUVENKATAREDDY <141143051+ALLUVENKATAREDDY@users.noreply.github.com>
2026-09-07 23:32:58 +0530

    Download NLTK punkt and stopwords
    Added NLTK downloads for punkt and stopwords.

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import streamlit as st
 import pickle
 import string
 import nltk
+
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
